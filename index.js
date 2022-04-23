f7e977439c0e68d2b14936595e8ff312094ccaac
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-23 15:32:06 +0100

    added bubbles

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector('.visual');
@@ -12,9 +12,21 @@ window.addEventListener('load', () => {
 ];
 
 pads.forEach((pad, index) => {
-    pad.addEventListener('click', function(){
+    pad.addEventListener("click", function(){
     sounds[index].currentTime = 0;
     sounds[index].play();
+
+    createBubble(index);
     });
 });
+
+const createBubble = index => {
+    const bubble = document.createElement("div");
+    visual .appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = 'jump 1s ease';
+    bubble.addEventListener('animationend', function(){
+        visual.removeChild(this);
+    })
+}
 });
