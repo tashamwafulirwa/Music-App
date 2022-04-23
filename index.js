29b90163221af89563f0fa1a0b713205c8b7c0d7
tashamwafulirwa <103854496+tashamwafulirwa@users.noreply.github.com>
2022-04-23 15:15:47 +0100

    update js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
-
+    const visual = document.querySelector('.visual');
+    const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3", 
+];
 
 pads.forEach((pad, index) => {
     pad.addEventListener('click', function(){
